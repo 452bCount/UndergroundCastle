ca516236638dac33a245699af777a0843f2f49cc
KKroz <krozflame@gmail.com>
2024-04-06 23:32:38 -0700

    Add Render Texture

--- Assets/MyAssets/Scripts/Trap/TrapElec.cs
+++ Assets/MyAssets/Scripts/Trap/TrapElec.cs
@@ -7,10 +7,10 @@ namespace MoleSurvivor
     public class TrapElec : Trap
     {
         public Transform colliderTransform;
-
         public List<Vector3> colliderMove;
-
         public float waitTime = 1.0f; // Delay between moves, in seconds
+
+        private Coroutine storeCoroutine;
 
         //protected override void StartCall(Transform cPlayer)
         //{
@@ -19,7 +19,7 @@ namespace MoleSurvivor
 
         public void Start()
         {
-            StartCoroutine(CorUpdate());
+            storeCoroutine = StartCoroutine(CorUpdate());
         }
 
         public IEnumerator CorUpdate()
@@ -27,6 +27,8 @@ namespace MoleSurvivor
             int index = 0; // Start at the first position
             while (true) // Loop indefinitely
             {
+                SetEnemyDestroy();
+
                 if (colliderMove.Count == 0)
                 {
                     yield break; // Exit if there are no positions to move to
@@ -34,6 +36,25 @@ namespace MoleSurvivor
                 colliderTransform.position = transform.position + colliderMove[index % colliderMove.Count]; // Move to the next position
                 index++; // Increment index for the next position
                 yield return new WaitForSeconds(waitTime); // Wait for the specified time before continuing
+            }
+        }
+
+        void SetEnemyDestroy()
+        {
+            // Assuming inGameCamera, horizontal, vertical, and _screenSpace are already defined
+            Vector3 centerPosition = InGameController.Instance.inGameCamera.position;
+            float innerTop = centerPosition.y + InGameController.Instance.vertical;
+            float outerTop = innerTop + InGameController.Instance._screenSpace.y;
+
+            if (transform.position.y > outerTop)
+            {
+                if (storeCoroutine != null)
+                {
+                    StopCoroutine(storeCoroutine);
+                    storeCoroutine = null; // Nullify the reference after stopping
+                }
+
+                Destroy(this.gameObject);
             }
         }
 
