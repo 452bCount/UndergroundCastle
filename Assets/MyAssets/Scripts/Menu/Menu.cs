cd6c60ac6ca9ad991c7fa8adbf0e033a4f700238
KKroz <krozflame@gmail.com>
2024-05-01 00:14:08 -0700

    Update Menu

---
+++ Assets/MyAssets/Scripts/Menu/Menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Method to load a new scene by name
+    public void LoadSceneName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Method to load a new scene by index
+    public void LoadSceneInt(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Method to quit the application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
