4939c5d9b4a5aac535a9a83155f3ec089bd2cc60
Siyuan452 (SAE perth) <1018131@student.sae.edu.au>
2024-05-02 21:09:07 +0800

    99% done, only need to add controller inputs on menu.
    Need to add controller inputs on menu to do scene transfer.

--- Assets/MyAssets/Scripts/Menu/OpenSubPanelOfStart.cs
+++ Assets/MyAssets/Scripts/Menu/OpenSubPanelOfStart.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpenSubPanelOfStart : MonoBehaviour
 {
@@ -8,5 +9,10 @@ public class OpenSubPanelOfStart : MonoBehaviour
     public void OpenSubPanel()
     {
         SubPanelOfStart.SetActive(true);
+    }
+
+    public void LoadSceneInt(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
